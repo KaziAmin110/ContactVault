3b0f8d798f576a284c857d750f540b5cebdbd5d6
Warren Snipes <contact@warrensnipes.dev>
2024-05-21 21:03:59 +0000

    Fixed init.sql

--- database_init_scripts/init.sql
+++ database_init_scripts/init.sql
@@ -1,26 +1,32 @@
 -- Current Database: `ContactManagerDB`
+CREATE DATABASE contact_manager;
 
-CREATE DATABASE 'ContactManagerDB';
-USE 'ContactManagerDB'
+USE contact_manager;
 
+CREATE TABLE
+  `users` (
+    `id` int NOT NULL AUTO_INCREMENT,
+    `date_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `date_last_logged_in` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `first_name` VARCHAR(50) NOT NULL,
+    `last_name` VARCHAR(50) NOT NULL,
+    `authentication_provider` ENUM ('GITHUB', 'GOOGLE', 'USERNAME_PASSWORD') NOT NULL,
+    `authentication_id` VARCHAR(256) NOT NULL, -- Will be the id given by authentication provider. For USERNAME_PASSWORD it will be the username.
+    `password` VARCHAR(50), -- Will be NULL when autentication_provider==USERNAME_PASSWORD,
+    PRIMARY KEY (id)
+  );
 
-CREATE TABLE `Users` (
-  `ID` int NOT NULL AUTO_INCREMENT,
-  `DateCreated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `DateLastLoggedIn` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `FirstName` varchar(50) NOT NULL DEFAULT '',
-  `LastName` varchar(50) NOT NULL DEFAULT '',
-  `Login` varchar(50) NOT NULL DEFAULT '',
-  `Password` varchar(50) NOT NULL DEFAULT '',
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-CREATE TABLE `Contacts` (
-  `ID` int NOT NULL AUTO_INCREMENT,
-  `FirstName` varchar(50) NOT NULL DEFAULT '',
-  `LastName` varchar(50) NOT NULL DEFAULT '',
-  `PhoneNumber` varchar(50) NOT NULL DEFAULT '',
-  `EmailAddress` varchar(50) NOT NULL DEFAULT '',
-  `UserID` int NOT NULL DEFAULT '0',
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE
+  `contacts` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `user_id` iNT NOT NULL,
+    `first_name` VARCHAR(50) NOT NULL,
+    `last_name` VARCHAR(50),
+    `phone_number` VARCHAR(50),
+    `email_address` VARCHAR(50),
+    `avatar_url` VARCHAR(512),
+    `bio` VARCHAR(2048),
+    `description` VARCHAR(2048),
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES users (id)
+  );
