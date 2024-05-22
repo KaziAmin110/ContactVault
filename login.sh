e7435ace658a0d8e0626c3d0cbdd76d24fd860f0
Warren Snipes <contact@warrensnipes.dev>
2024-05-22 17:41:41 +0000

    fixed path

--- login.sh
+++ login.sh
@@ -1,4 +1,4 @@
-curl -X POST http://localhost:80/api/register.php \
+curl -X POST http://localhost:80/api/login.php \
 -H "Content-Type: application/json" \
 -d '{
     "authentication_provider": "USERNAME_PASSWORD",
