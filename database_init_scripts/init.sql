0a3f5a15b43c8076f06684e963528854744f0f5d
Warren Snipes <contact@warrensnipes.dev>
2024-05-22 00:08:43 +0000

    removed first_name and last_name, and added password

--- database_init_scripts/init.sql
+++ database_init_scripts/init.sql
@@ -8,11 +8,11 @@ CREATE TABLE
     `id` int NOT NULL AUTO_INCREMENT,
     `date_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
     `date_last_logged_in` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `first_name` VARCHAR(50) NOT NULL,
-    `last_name` VARCHAR(50) NOT NULL,
+--    `first_name` VARCHAR(50) NOT NULL,
+--    `last_name` VARCHAR(50) NOT NULL,
     `authentication_provider` ENUM ('GITHUB', 'GOOGLE', 'USERNAME_PASSWORD') NOT NULL,
     `authentication_id` VARCHAR(256) NOT NULL, -- Will be the id given by authentication provider. For USERNAME_PASSWORD it will be the username.
-    `password` VARCHAR(50), -- Will be NULL when autentication_provider==USERNAME_PASSWORD,
+    `password` VARCHAR(512), -- Will be NULL when autentication_provider==USERNAME_PASSWORD,
     PRIMARY KEY (id)
   );
 
