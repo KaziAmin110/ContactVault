ecba86f6ba70c6499ee8e43c7cbae3f54971105f
Warren Snipes <contact@warrensnipes.dev>
2024-05-22 18:14:22 +0000

    fixed file path

--- app/src/api/config.php
+++ app/src/api/config.php
@@ -6,6 +6,6 @@ define('DB_USER', getenv("MYSQL_USER"));
 define('DB_PASS', getenv("MYSQL_PASSWORD"));
 define('DB_NAME', getenv("MYSQL_DATABASE"));
 
-define('PRIVATE_KEY', file_get_contents('/secrets/private_key.pem'));
-define('PUBLIC_KEY', file_get_contents('/secrets/public_key.pem'));
+define('PRIVATE_KEY', file_get_contents('/secrets/jwt_encryption_key.pem'));
+define('PUBLIC_KEY', file_get_contents('/secrets/jwt_encryption_key.pem.pub'));
 ?>
