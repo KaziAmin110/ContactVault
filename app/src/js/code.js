2b7336f9c1c906e818c6e38fc943a7fe1dfe7ebe
jandro damas <jandroschool@gmail.com>
2024-05-26 20:26:39 +0000

    sign up with form varification

--- app/src/js/code.js
+++ app/src/js/code.js
@@ -1,6 +1,8 @@
 //change hosting
 const urlBase = 'http://localhost/';
 const extension = 'php';
+const usernameRegex = /^[a-zA-Z0-9_]+$/;
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
 let userId = 0;
 let firstName = "";
@@ -19,6 +21,129 @@ function doLogout()
 	lastName = "";
 	document.cookie = "firstName= ; expires = Thu, 01 Jan 1970 00:00:00 GMT";
 	window.location.href = "../../index.html";
+}
+
+function signUp(){
+	event.preventDefault();
+	let res= isValidRegister();
+
+	document.getElementById("validRegisterResult").innerHTML=res;
+	
+	if(res=="")
+		register();
+}
+
+function isValidPassword(password){
+
+	let passwordRequieres="";
+
+	const minLength = 8;
+    
+	const hasUppercase = /[A-Z]/.test(password);
+	const hasLowercase = /[a-z]/.test(password);
+	const hasNumber = /[0-9]/.test(password);
+	const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+ 
+	const isValidLength = password.length >= minLength;
+
+	if(!isValidLength)
+		passwordRequieres+= "Password must be atleast 8 characters long<br>"
+	if(!hasUppercase)
+		passwordRequieres+= "Password must contain atleast 1 uppercase<br>"
+	if(!hasLowercase)
+		passwordRequieres+= "Password must contain atleast 1 lowercase<br>"
+	if(!hasNumber)
+		passwordRequieres+= "Password must contain atleast 1 number<br>"
+	if(!hasSpecialChar)
+		passwordRequieres+= "Password must contain atleast 1 special character<br>"
+	
+	return passwordRequieres;
+}
+
+function isValidRegister(){
+
+	//if the user has input a field incorrectly, message will let the user know what is wrong
+	let message= "";
+	
+	let first_name = document.getElementById("first-name").value;
+	let last_name = document.getElementById("last-name").value;
+	let email = document.getElementById("email").value;
+	let userName= document.getElementById("user-name").value;
+	let password = document.getElementById("password").value
+
+	if(first_name.length ==0 ){
+		message+="First name cannot be empty<br>";
+		flag=false;
+	}
+
+	if(last_name.length ==0 ){
+		message+="Last name cannot be empty<br>";
+		flag=false;
+	}
+
+	if(!usernameRegex.test(userName)){
+		message+="Invalid username<br>";
+		message+="please make sure the username is between 3-20 characters and has only numbers, letters, and _<br>";
+		flag=false;
+	}
+
+	if(!emailRegex.test(email) ){
+		message+="Invalid email<br>";
+		flag=false;
+	}
+
+	let passReq= isValidPassword(password);
+
+	if(passReq!=""){
+		message+=passReq+"<br>";
+		flag=false;
+	}
+
+	return message;
+}
+
+//not complete
+function register(){
+	
+	let first_name = document.getElementById("first-name").value;
+	let last_name = document.getElementById("last-name").value;
+	let email = document.getElementById("email").value;
+	let userName= document.getElementById("user-name").value;
+	let password = document.getElementById("password").value
+
+	//needs to be updated to use google
+	let tmp={enum: "USERNAME_PASSWORD",username:userName,password:password};
+
+	let jsonPayload= JSON.stringify(tmp);
+	let url= urlBase + "api/register"+extension;
+	
+	let xhr = new XMLHttpRequest();
+
+	xhr.open("POST", url, true);
+
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	
+	try{
+		xhr.onreadystatechange= function() {
+
+			if(this.readyState==4 && this.status == 200){
+
+				let jsonObject= JSON.parse(xhr.responseText);
+
+				if(jsonObject.console.error != ''){
+					document.getElementById("registerResult").innerHTML="An error has occured";
+				}
+			}
+			else{
+				document.getElementById("registerResult").innerHTML= "Successfully registered!";
+				window.location.href = "contacts-page/index.html";
+			}
+		};
+		xhr.send(jsonPayload);
+	}
+	catch(err){
+		document.getElementById("registerResult").innerHTML=err.message;
+	}
 }
 
 //noty working error 404
