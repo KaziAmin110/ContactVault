64699d105f031938e948b9ff986b664f023df237
Warren Snipes <contact@warrensnipes.dev>
2024-05-27 21:03:33 +0000

    updated register.sh script to reflect changes

--- register.sh
+++ register.sh
@@ -3,6 +3,8 @@ curl -X POST http://localhost:80/api/register.php \
 -d '{
     "authentication_provider": "USERNAME_PASSWORD",
     "username": "your_username",
-    "password": "your_password"
+    "password": "your_password",
+    "first_name": "Joe",
+    "last_name": "Mama"
 }'
 
