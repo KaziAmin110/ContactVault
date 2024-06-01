e387ce315de8ee693d0b7a08f112608abe5f627d
Kazi Amin <126922373+KaziAmin110@users.noreply.github.com>
2024-06-01 19:47:08 +0000

    Fixed Edit Button Display Issue

--- app/src/contacts-page/contacts.js
+++ app/src/contacts-page/contacts.js
@@ -312,8 +312,11 @@ function addNewContact() {
         .then(contactId => {
             // Creates a dynamic instance of a contact using all of the information from the popout
             const newContactItem = document.createElement('li');
+            const idDiv = document.createElement('div');
+            idDiv.innerText = `${contactId}`;
+            idDiv.style.display = "none";
             newContactItem.classList.add('list-group-item');
-            newContactItem.setAttribute('data-id', contactId);
+            newContactItem.appendChild(idDiv);
             newContactItem.setAttribute('onclick', `selectContact(this, '${firstname}', '${lastname}', '${email}', '${phone}', '${avatarDataUrl}', '${bio}', '${description.value}')`);
             newContactItem.innerHTML = `
                 <div class="contact-info">
